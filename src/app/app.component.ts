cd4991245f8b3f4c64f558bd570b37204fb4d6ea
Yovani <yovani.guel@cceo.com.mx>
2017-11-14 17:44:41 -0600

    Se creo una nueva carpeta llamada pipes y se creo una tabla y se le estan aplicando los pipes uppercase, lowercase y slice a un arreglo y a un nombre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  nombre= "Yovani";
+
+  arreglo=[1,2,3,4,5,6,7,8,9,10];
 }
