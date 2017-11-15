e68e9a4b8f31f603398f6fa4ccad0c765601d1e6
Yovani <yovani.guel@cceo.com.mx>
2017-11-14 18:56:16 -0600

    Se agrego el pipe para capitalizar las letras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre= "Yovani";
+  nombre2= "yoVani dAniEl gUel gOmEz"
 
   arreglo=[1,2,3,4,5,6,7,8,9,10];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {LOCALE_ID} from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {CapitalizadoPipe} from './pipes/capitalizado.pipe';
 
 @NgModule({
   declarations: [
+    CapitalizadoPipe,
     AppComponent
   ],
   imports: [
